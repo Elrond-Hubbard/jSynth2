845ae3ff31343aa0f792e8d0d841ba11a674fb8f
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-23 19:17:42 -0400

    added keyUp event

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,31 @@
 
-// ELEMENT ASSIGNMENTS
-const playButton = document.getElementById("play-button");
 
-// CREATE SYNTH
+// SYNTH
 const synth = new Tone.Synth().toDestination();
 const osc2 = new Tone.Synth({detune: 12}).toDestination();
 const osc3 = new Tone.Synth({detune: -12}).toDestination();
 
-
-// CREATE CONTROLLER
+// KEYBOARD CONTROLLER
 const keyboard = new AudioKeys({
     rows: 2,
+    priority: 'last'
 });
-
 keyboard.down((key) => {
-    synth.triggerAttackRelease(key.frequency, "4n");
-    osc2.triggerAttackRelease(key.frequency, "4n");
-    osc3.triggerAttackRelease(key.frequency, "4n");
+    synth.triggerAttack(key.frequency);
+    osc2.triggerAttack(key.frequency);
+    osc3.triggerAttack(key.frequency);
 })
 
-playButton.addEventListener("click", () => {
-    if (Tone.context.state !== "running") {
+keyboard.up(() => {
+    synth.triggerRelease();
+    osc2.triggerRelease();
+    osc3.triggerRelease();
+})
+
+// INIT AUDIO CONTEXT
+const playButton = document.getElementById("play-button");
+playButton.addEventListener('click', () => {
+    if (Tone.context.state !== 'running') {
         Tone.start();
     }
 })
