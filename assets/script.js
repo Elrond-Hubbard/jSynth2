ae8f6707f049f59ce18e9a2a4ac7ed8a0607b2af
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-09-17 18:37:37 -0400

    massive refactor and cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // SYNTH
+
 const comp = new Tone.Compressor(-50, 3).toDestination();
 const synth = new Tone.MonoSynth({
     oscillator: {
@@ -26,97 +27,76 @@ const synth = new Tone.MonoSynth({
     }
 }).connect(comp);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// OSCILLATOR
 
-// WAVEFORM SELECTOR
+// SHAPE BUTTONS
 const waveformButtons = document.querySelectorAll('input[name="waveform"]')
 waveformButtons.forEach(button =>
     button.addEventListener('input', function () {
         synth.oscillator.type = button.value;
     }
 ));
-
-// UNISON CONTROLS
+// UNISON BUTTONS
 const countButtons = document.querySelectorAll('input[name="count"]')
 countButtons.forEach(button =>
     button.addEventListener('input', function () {
         let count = parseInt(button.value)
         synth.oscillator.count = count;
     }))
-const unisonSlider = document.querySelector('#spread')
-unisonSlider.addEventListener('input', function () {
-    let spread = parseInt(unisonSlider.value);
-    synth.oscillator.spread = spread;
-})
 
-// ENVELOPE CONTROLS
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// SLIDER CONTROLS
+
+function slider(element, object, property) {
+    element.addEventListener('input', function () {
+      let value = parseFloat(this.value);
+      object[property] = value;
+    });
+  }
+
+// UNISON SLIDER
+const unisonSlider = document.querySelector('#spread')
+slider(unisonSlider, synth.oscillator, 'spread')
+
+// AMP ENVELOPE SLIDERS
 const attackSlider = document.querySelector('#attack');
 const decaySlider = document.querySelector('#decay');
 const sustainSlider = document.querySelector('#sustain');
 const releaseSlider = document.querySelector('#release');
+slider(attackSlider, synth.envelope, 'attack')
+slider(decaySlider, synth.envelope, 'decay')
+slider(sustainSlider, synth.envelope, 'sustain')
+slider(releaseSlider, synth.envelope, 'release')
 
+// FILTER ENVELOPE SLIDERS
 const filterAttackSlider = document.querySelector('#filter-attack');
 const filterDecaySlider = document.querySelector('#filter-decay');
 const filterSustainSlider = document.querySelector('#filter-sustain');
 const filterReleaseSlider = document.querySelector('#filter-release');
-
-// AMP ENVELOPE
-attackSlider.addEventListener('input', function () {
-    let attack = parseFloat(this.value);
-    synth.envelope.attack = attack;
-})
-decaySlider.addEventListener('input', function () {
-    let decay = parseFloat(this.value);
-    synth.envelope.decay = decay;
-})
-sustainSlider.addEventListener('input', function () {
-    let sustain = parseFloat(this.value);
-    synth.envelope.sustain = sustain;
-})
-releaseSlider.addEventListener('input', function () {
-    let release = parseFloat(this.value);
-    synth.envelope.release = release;
-})
-
-// FILTER ENVELOPE
-filterAttackSlider.addEventListener('input', function () {
-    let attack = parseFloat(this.value);
-    synth.filterEnvelope.attack = attack;
-})
-filterDecaySlider.addEventListener('input', function () {
-    let decay = parseFloat(this.value);
-    synth.filterEnvelope.decay = decay;
-})
-filterSustainSlider.addEventListener('input', function () {
-    let sustain = parseFloat(this.value);
-    synth.filterEnvelope.sustain = sustain;
-})
-filterReleaseSlider.addEventListener('input', function () {
-    let release = parseFloat(this.value);
-    synth.filterEnvelope.release = release;
-})
-
-
+slider(filterAttackSlider, synth.filterEnvelope, 'attack')
+slider(filterDecaySlider, synth.filterEnvelope, 'decay')
+slider(filterSustainSlider, synth.filterEnvelope, 'sustain')
+slider(filterReleaseSlider, synth.filterEnvelope, 'release')
 
 // FILTER CONTROLS
 cutoffSlider = document.querySelector('#cutoff')
 resonanceSlider = document.querySelector('#resonance')
-cutoffSlider.addEventListener('input', function() {
-    let cutoff = parseFloat(this.value);
-    synth.filterEnvelope.baseFrequency = cutoff;
-})
-resonanceSlider.addEventListener('input', function() {
-    let resonance = parseFloat(this.value);
-    synth.filter.Q.value = resonance;
-})
+slider(cutoffSlider, synth.filterEnvelope, 'baseFrequency')
+slider(resonanceSlider, synth.filter.Q, 'value')
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // KEYBOARD CONTROLLER
+
 const keyboard = new AudioKeys({
     rows: 2,
     polyphony: 1,
     priority: 'last',
     octave: -2
 });
+
+let currentKeyDown;
+let currentKeyUp;
 keyboard.down((note) => {
     currentKeyDown = note.note;
     synth.triggerAttack(note.frequency);
@@ -131,34 +111,70 @@ keyboard.up((note) => {
     if (currentKeyUp === currentKeyDown) { synth.triggerRelease() };
 });
 
-// LOOPS
-// hardcoded loop
-const shredBass = [
-    "A2", "A2", "C2", "D2",
-    "A1", "A1", "C2", "D2",
-    "A1", "A1", "C2", "D2",
-    "A1", "A1", "C2", "D2",
-    "G2", "G2", "B1", "C2",
-    "G1", "G1", "B1", "C2",
-    "G1", "G1", "B1", "C2",
-    "G1", "G1", "B1", "C2",
-];
-// dynamic loop
-var dynamicLoop = [];
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// TRANSPORT
 
+Tone.Transport.loop = true;
+Tone.Transport.loopStart = 0;
+Tone.Transport.loopEnd = "1m"
+var beat = 1
+Tone.Transport.scheduleRepeat((time) => {
+    console.log(`${beat}!`);
+    if (beat >= 16) {
+        beat = 1
+        sequence.events = dynamicLoop
+    } else {
+        beat++
+    }
+}, "16n", 0)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// LOOPS
+
+var dynamicLoop = [];
+// example loops
+const loop1 = [
+    "A1", "A1", "A1", "A2", 
+    "A1", "A1", "A1", "A2", 
+    "A1", "A1", "A1", "A2", 
+    "A1", "A1", "C2", "C2", 
+]
+const loop2 = [
+    "G1", "A2", "B2", "C2", 
+    "G1", "A2", "B2", "C2", 
+    "G1", "A2", "B2", "C2", 
+    "G1", "A2", "G2", "G2", 
+]
+function loopHandler(element, loop) {
+    element.addEventListener('click', () => {
+        dynamicLoop = loop;
+        sequenceLabel.innerText = dynamicLoop;
+    })
+}
+const loop1Button = document.getElementById("loop1-button")
+const loop2Button = document.getElementById("loop2-button")
+loopHandler(loop1Button, loop1)
+loopHandler(loop2Button, loop2)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // SEQUENCER
+
 const sequenceLabel = document.getElementById("sequence-label")
 const sequence = new Tone.Sequence((time, note) => {
     synth.triggerAttackRelease(note, "16n", time)
-    }, shredBass, "16n");
+    }, dynamicLoop, "16n");
+
 // PLAY BUTTON
 const playButton = document.getElementById("play-button");
 playButton.addEventListener('click', () => {
     playButton.style.backgroundColor = "green"
     playButton.style.color = "white"
     sequence.events = dynamicLoop;
-    sequence.start();
-    Tone.Transport.start();
+    sequence.start(0);
+    // kickSequence.start(0)
+    // snareSequence.start(0)
+    Tone.Transport.start(0);
+    beat = 1
 });
 // STOP BUTTON
 const stopButton = document.getElementById("stop-button");
@@ -166,6 +182,8 @@ stopButton.addEventListener('click', () => {
     playButton.style.backgroundColor = ""
     playButton.style.color = ""
     sequence.stop(0)
+    // kickSequence.stop(0)
+    // snareSequence.stop(0)
     Tone.Transport.stop(0)
 })
 // CLEAR BUTTON
@@ -200,14 +218,14 @@ bpmForm.addEventListener('input', () => {
     }
 })
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // OSCILLOSCOPE
 const canvas = document.getElementById("canvas");
 const canvasWidth = canvas.offsetWidth;
 const ctx = canvas.getContext("2d");
 
 const toneAnalyser = new Tone.Analyser({size: 2048, type: "waveform"});
-synth.connect(toneAnalyser)
+comp.connect(toneAnalyser)
 
 function draw() {
     requestAnimationFrame(draw)
@@ -224,7 +242,7 @@ function draw() {
 
     for (i = 0; i < oscBuffer; i++) {
         const v = oscArray[i] / 16.0;
-        const y = v * (200);
+        const y = v * (400);
         if (i === 0) {
             ctx.moveTo(x/2-3, y);
         } else {
@@ -237,3 +255,14 @@ function draw() {
     ctx.stroke();
 }
 draw()
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const kick = new Tone.MembraneSynth().connect(comp)
+// const snare = new Tone.NoiseSynth().connect(comp)
+// const kickSequence = new Tone.Sequence((time, note) => {
+//     kick.triggerAttackRelease(note, "4n", time)
+//     }, ["C2", "C2", "C2", "C2"], "4n");
+// const snareSequence = new Tone.Sequence((time, ) => {
+//     snare.triggerAttackRelease("4n", time)
+//     }, [null, "C2", null, "C2"], "4n");
